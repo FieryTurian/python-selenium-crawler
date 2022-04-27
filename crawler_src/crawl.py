669537094fce3d60a263dfd8c60ca4931b1b0eaf
LKolijn <laurakolijn@gmail.com>
2022-04-27 22:09:32 +0200

    Added a function to set Chrome options + screenshot functionality

--- crawler_src/crawl.py
+++ crawler_src/crawl.py
@@ -3,6 +3,7 @@
 A placeholder for a very nice description of our crawler :)
 """
 import argparse
+import time
 
 import pandas as pd
 
@@ -10,6 +11,9 @@ from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+WINDOW_SIZE = "1920x1080"
 
 
 def parse_arguments():
@@ -56,13 +60,51 @@ def read_tranco_top_500(file_path):
     return tranco_dict
 
 
-def crawl_url(url):
-    driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
+def set_webdriver_options(params):
+    """Set the correct options for the Chrome webdriver
+
+    Parameters
+    ----------
+    params: dict
+        A dictionary with the values for all command line arguments
+
+    Returns
+    -------
+    selenium.webdriver.chrome.options.Options
+        ChromeOptions that are used to customize the ChromeDriver session
+    """
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--start-maximized')
+    # To remove the "Chrome is being controlled by automated test software" notification:
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+
+    if params['view'] == "headless":
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument(f'--window-size={WINDOW_SIZE}')
+
+    if params['mobile']:
+        mobile_emulation = {"deviceName": "iPhone X"}
+        chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    return chrome_options
+
+
+def crawl_url(url, params):
+    chrome_options = set_webdriver_options(params)
+    driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe", chrome_options=chrome_options)
+    # driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe", chrome_options=chrome_options)
 
     website_domain = "https://" + url
     driver.get(website_domain)
+    time.sleep(10)
+
+    if params['mobile']:
+        driver.save_screenshot(f'./{url}_mobile_pre_consent.png')
+    else:
+        driver.save_screenshot(f'./{url}_desktop_pre_consent.png')
 
     requests = driver.requests
+    driver.quit()
 
     # # link for switching to English
     # en_link = driver.find_element(
@@ -84,9 +126,9 @@ def crawl_url(url):
     return
 
 
-def crawl_list(domain_list):
+def crawl_list(domain_list, params):
     for domain in domain_list.values():
-        crawl_url(domain)
+        crawl_url(domain, params)
 
     print("Please give us a moment, we are trying to crawl your entire input list :D")
     return
@@ -96,9 +138,9 @@ if __name__ == '__main__':
     args = parse_arguments()
     if args['input']:
         tranco_domains = read_tranco_top_500(args['input'])
-        crawl_list(tranco_domains)
+        crawl_list(tranco_domains, args)
 
     if args['url']:
-        crawl_url(args['url'])
+        crawl_url(args['url'], args)
 
     print("Hello world!")
