8dd8b484d627d19d4192b9877a348b507b24c55e
Onno <emailodg@gmail.com>
2022-04-28 14:41:43 +0200

    Used code fragment from week4 example

--- crawler_src/crawl.py
+++ crawler_src/crawl.py
@@ -107,24 +107,24 @@ def crawl_url(url, params):
         driver.save_screenshot(f'../crawl_data/{url}_desktop_pre_consent.png')
 
     requests = driver.requests
-    driver.quit()
 
-    # # link for switching to English
-    # en_link = driver.find_element(
-    #     By.CSS_SELECTOR, "li.language:nth-child(2) > span:nth-child(2) > a:nth-child(1)")
-    # en_link.click()
-    #
-    # # we now want to click the Allow all cookies button
-    # # The following fails sine the allow all button takes a while to load
-    # # driver.find_element(By.CLASS_NAME, "btn_alow_true")
-    # # instead we wait until it becomes clickable
-    # allow_all = WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.CLASS_NAME, "btn_allow_true")))
-    #
-    # if allow_all:
-    #     allow_all.click()
-    # driver.quit()
-    # print("I am here :D")
+    # link for switching to English
+    en_link = driver.find_element(
+        By.CSS_SELECTOR, "li.language:nth-child(2) > span:nth-child(2) > a:nth-child(1)")
+    en_link.click()
+
+    # we now want to click the Allow all cookies button
+    # The following fails sine the allow all button takes a while to load
+    # driver.find_element(By.CLASS_NAME, "btn_alow_true")
+    # instead we wait until it becomes clickable
+    allow_all = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "btn_allow_true")))
+
+    if allow_all:
+        allow_all.click()
+
+    driver.quit()
+    print("I am here :D")
 
     return requests
 
