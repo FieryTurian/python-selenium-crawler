abe70327cdf3719dafd24e458b3ae7081b101fa3
LKolijn <laurakolijn@gmail.com>
2022-05-21 18:13:12 +0200

    Change structure + add customization for boxplots

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -3,10 +3,12 @@
 Analyse the data obtained by the crawler:
 [WORK IN PROGRESS]
 """
+from colors import *
 import csv
 import glob
 import json
 import os
+import sys
 from ast import literal_eval
 
 import pandas as pd
@@ -25,7 +27,7 @@ def write_data_to_csv(headers):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     # Get all JSON files within the crawl_data folder
-    files = glob.glob(f"../crawl_data/*.json")
+    files = glob.glob("../crawl_data/*.json")
     data = []
     for file in files:
         with open(file, 'r') as f:
@@ -43,7 +45,7 @@ def write_data_to_csv(headers):
 
     # Add headers
     data.insert(0, headers)
-    with open("data.csv", 'w', newline="") as f:
+    with open("data/data.csv", 'w', newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
@@ -61,7 +63,7 @@ def csv_to_pandas_dataframe(headers):
     dataframe: pandas.core.series.Series
         A Pandas dataframe with all the data in the CSV file
     """
-    dataframe = pd.read_csv("data.csv", usecols=headers)
+    dataframe = pd.read_csv("data/data.csv", usecols=headers)
 
     # Turn strings into their corresponding python literals
     dataframe['third_party_domains'] = dataframe.third_party_domains.apply(literal_eval)
@@ -70,7 +72,66 @@ def csv_to_pandas_dataframe(headers):
     return dataframe
 
 
-def generate_box_plot(dataframe, header, crawl_mode):
+def customize_grid(ax, border, yaxis, xaxis, bg_color='white', grid_color='gray', width=1.2, yminor=True, xminor=True):
+    """Customize the looks of the grid
+
+    Parameters
+    ----------
+    ax: matplotlib.axes._subplots.AxesSubplot
+        The matplotlib axes instance the plot is drawn on
+    border: bool
+        A Boolean value that defines whether or not the border needs to be drawn
+    yaxis, xaxis: bool
+        Boolean values that define whether respectively the horizontal and vertical
+         (mayor) grid lines need to be drawn
+    bg_color: str, default='white'
+        The background color of the grid
+    grid_color: str, default='gray'
+        The color of the grid lines
+    width: float, default=1.2
+        The width of the grid lines
+   yminor, xminor: bool, default=True
+       Boolean values that define whether respectively the horizontal and vertical
+        minor grid lines need to be drawn
+    """
+    ax.set_facecolor(bg_color) # Set background color
+    [ax.spines[spine].set_visible(border) for spine in ax.spines] # Remove border around the grid
+    if yaxis:
+        ax.yaxis.grid(yaxis, which='major', color=grid_color, linewidth=width)
+    if yminor:
+        ax.yaxis.grid(yminor, which='minor', color=grid_color, linewidth=width/4)
+        ax.minorticks_on()
+        ax.tick_params(which='minor', bottom=False, left=False)
+    if xaxis:
+        ax.xaxis.grid(xaxis, which='major', color=grid_color, linewidth=width)
+    if xminor:
+        ax.xaxis.grid(xminor, which='minor', color=grid_color, linewidth=width/4)
+        ax.minorticks_on()
+        ax.tick_params(which='minor', bottom=False, left=False)
+    ax.set(axisbelow=True) # Do not draw the grid over the plotted items
+
+
+def customize_box_plot_color(ax):
+    """Customize the colors of all boxplot parts
+
+    Parameters
+    ----------
+    ax: matplotlib.axes._subplots.AxesSubplot
+        The matplotlib axes instance the plot is drawn on
+    """
+    for i, box in enumerate(ax['boxes']):
+        box.set(facecolor=BOX_FACECOLOR[i], edgecolor=BOX_EDGECOLOR[i])
+    for whisker in ax['whiskers']:
+        whisker.set(color=BOX_LINE, linestyle=':')
+    for cap in ax['caps']:
+        cap.set_color(BOX_LINE)
+    for median in ax['medians']:
+        median.set(color=BOX_MEDIAN)
+    for flier in ax['fliers']:
+        flier.set(color=BOX_EDGECOLOR[i], marker='*')
+
+
+def generate_box_plot(dataframe, header, crawl_mode, metric):
     """Read the CSV file into a pandas dataframe in order to analyse the data
 
     Parameters
@@ -81,9 +142,17 @@ def generate_box_plot(dataframe, header, crawl_mode):
         A string with the value for one of the headers of the data in the dataframe
     crawl_mode: string
         A string with the value for the crawl_mode header of the data in the dataframe
+    metric: string
+        A string with the metric that is plotted
     """
-    dataframe.boxplot(header, by=crawl_mode)
-    plt.savefig(f"box_plot_{header}.png")
+    bp_dict = dataframe.boxplot(header, grid=False, by=crawl_mode, return_type='both', patch_artist=True)
+    customize_grid(bp_dict[0][0], False, True, False, BOX_BACKGROUND, BOX_GRID, xminor=False)
+    customize_box_plot_color(bp_dict[0][1])
+    plt.title(f"The distribution of the {metric} per website\nfor both desktop and mobile crawl mode.")
+    plt.suptitle("") # Remove the automatic "grouped by" title
+    plt.xlabel("Crawl mode")
+    plt.ylabel(metric.capitalize())
+    plt.savefig(f"data/box_plot_{header}.png", bbox_inches='tight')
     plt.show()
 
 
@@ -91,8 +160,9 @@ def main():
     headers = ["website_domain", "crawl_mode", "third_party_domains", "nr_requests", "requests_list"]
     write_data_to_csv(headers)
     dataframe = csv_to_pandas_dataframe(headers)
-    generate_box_plot(dataframe, "nr_requests", "crawl_mode")
+    generate_box_plot(dataframe, "nr_requests", "crawl_mode", "number of requests")
 
 
 if __name__ == '__main__':
+    sys.dont_write_bytecode = True
     main()
---
+++ analysis/colors.py
@@ -0,0 +1,6 @@
+BOX_BACKGROUND = '#EBEBEB'
+BOX_GRID = 'white'
+BOX_FACECOLOR = ['lightblue', 'lightgreen']
+BOX_EDGECOLOR = ['dodgerblue', 'limegreen']
+BOX_LINE = 'black'
+BOX_MEDIAN = 'red'
